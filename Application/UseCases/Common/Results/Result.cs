988a7a2d0c52c0f2a4b12e96a730c8a6401fa8c0
Felix Zamora <flxzamora91@gmail.com>
2024-09-28 17:12:34 -0600

    Se estableciero las clases UseCaseHandler, Result y Resulttype para mostrar los resultados de las operaciones

---
+++ Application/UseCases/Common/Results/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Application.UseCases.Common.Results
+{
+    public class Result<T>(T data, ResultType resultType, params string[] errors)
+    {
+        public ResultType ResultType { get; } = resultType;
+
+        public IEnumerable<string> Errors { get; } = errors;
+
+        public T Data { get; } = data;
+
+    }
+}
